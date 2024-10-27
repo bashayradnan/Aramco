dde4768916ad0c885c43c70355a9ae4b4e92d86c
bashayradnan <157901380+bashayradnan@users.noreply.github.com>
2024-10-27 11:02:43 +0300

    Update untitled1.py

--- untitled1.py
+++ untitled1.py
@@ -1,15 +1,19 @@
 import streamlit as st
 from PIL import Image
 
-logo_path = "/Users/bashayralbinsaad/Downloads/New Project-5.png"
-logo = Image.open(logo_path)
+# logo_path = "New Project-5.png"  
+# logo = Image.open(logo_path)
 
-st.image(logo, width=100)
+
+# st.image(logo, width=100)
 
 st.title("Interactive Question System")
+st.write("Welcome to the data quality assessment tool!")
+
 
 answers = []
 
+# الأسئلة
 questions = [
     {"question": "How many data points do you have?", "type": "text"},
     {"question": "Is the data structured?", "type": "choice", "options": ["Yes", "No"]},
@@ -17,11 +21,13 @@ questions = [
     {"question": "Did you perform any cleaning?", "type": "choice", "options": ["Yes", "No"]},
 ]
 
+
 def get_result(answers):
     if answers[1] == "Yes" and answers[2] == "Yes" and answers[3] == "Yes":
         return "High-Quality Data"
     else:
         return "Low-Quality Data"
+
 
 for i, q in enumerate(questions):
     if q["type"] == "text":
@@ -30,6 +36,8 @@ for i, q in enumerate(questions):
         answer = st.radio(q["question"], q["options"])
     answers.append(answer)
 
+
 if st.button("Show Result"):
     result = get_result(answers)
     st.success(f"Your data is: {result}")
+
